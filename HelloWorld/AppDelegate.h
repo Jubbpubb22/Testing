de2318542bad747a8f9da6f48e27c4d46e209e9e
Joanna Bartolome <betterwings22@gmail.com>
2015-09-14 22:04:53 -0700

    Initial Commit

---
+++ HelloWorld/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  HelloWorld
+//
+//  Created by Joanna Bartolome on 9/14/15.
+//  Copyright (c) 2015 com.rypress. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface AppDelegate : NSObject <NSApplicationDelegate>
+
+
+@end
+
---
+++ HelloWorld/main.m
@@ -0,0 +1,13 @@
+//
+//  main.m
+//  HelloWorld
+//
+//  Created by Joanna Bartolome on 9/14/15.
+//  Copyright (c) 2015 com.rypress. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+int main(int argc, const char * argv[]) {
+    return NSApplicationMain(argc, argv);
+}
